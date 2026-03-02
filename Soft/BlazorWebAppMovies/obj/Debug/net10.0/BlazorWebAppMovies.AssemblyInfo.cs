c85848c4f4573acf36af977de7f14a5172e26ed8
heido <heidom@taltech.ee>
2026-03-02 22:57:26 +0200

    Äkki nüüd salvestub

--- Soft/BlazorWebAppMovies/obj/Debug/net10.0/BlazorWebAppMovies.AssemblyInfo.cs
+++ Soft/BlazorWebAppMovies/obj/Debug/net10.0/BlazorWebAppMovies.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorWebAppMovies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18b5dd56d1aa9a19b054d39a96b4577679b4891d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b2380537ab2a1af07541598646abc86e886c749")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorWebAppMovies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorWebAppMovies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
